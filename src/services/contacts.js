4268b6b9377672f684af5e8d193c146814e417fb
SAILOR <muhammetuzunoglu@gmail.com>
2025-02-05 00:06:07 +0300

    new branch 4. commit

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,10 +1,13 @@
 import Contact from '../db/Contact.js';
 
-// Eksik olan getContacts fonksiyonunu ekledik
 export const getContacts = async (req, res) => {
   try {
     const contacts = await Contact.find();
-    res.status(200).json(contacts);
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
   } catch (error) {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
@@ -14,12 +17,14 @@ export const getContactById = async (req, res) => {
   try {
     const { contactId } = req.params;
     const contact = await Contact.findById(contactId);
-
     if (!contact) {
       return res.status(404).json({ message: 'Contact not found' });
     }
-
-    res.status(200).json(contact);
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
   } catch (error) {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
